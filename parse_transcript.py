4b6ae63486554c26982fef1c425ca278ba3f7226
Christopher Groskopf <staringmonkey@gmail.com>
2013-01-02 16:22:11 -0600

    Cross-reference audio.

--- parse_transcript.py
+++ parse_transcript.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python
 
 import json
+import os
 from xml.etree import ElementTree
 
 import requests
 
 SEAMUS_ID = '166217427'
-API_URL = 'http://artemis.npr.org/dma/api/stories/seamus/%s' % SEAMUS_ID
+ARTEMIS_API_URL = 'http://artemis.npr.org/dma/api/stories/seamus/%s' % SEAMUS_ID
 
-response = requests.get(API_URL)
+NPR_API_KEY = os.environ.get('NPR_API_KEY')
+NPR_API_URL = 'http://api.npr.org/query?id=%s&dateType=story&output=JSON&apiKey=%s' % (SEAMUS_ID, NPR_API_KEY)
 
-data = json.loads(response.content)
+response = requests.get(ARTEMIS_API_URL)
 
-transcript = ElementTree.fromstring(data['hits'][0]['_source']['transcript_text'][0])
+data = json.loads(response.content)['hits'][0]['_source']
 
-output = []
+transcript = ElementTree.fromstring(data['transcript_text'][0])
+
+output = {
+    'title': data['story_title'],
+    'fragments': []
+}
 
 for fragment in transcript.iter('Fragment'):
-    output.append({
+    output['fragments'].append({
         'start_time': fragment.get('StartTime'),
         'text': fragment.text
     })
- 
+
+response = requests.get(NPR_API_URL)
+
+data = json.loads(response.content)
+
+output['mp3_url'] = data['list']['story'][0]['audio'][0]['format']['mp3'][0]['$text']
+
 with open('www/transcript.js', 'w') as f:
     json.dump(output, f)
+
